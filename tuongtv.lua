43841dda52f0a31fc13acf0daf3018937e09985a
thanh6a5dep-maker <thanh6a5dep@gmail.com>
2026-06-30 07:21:43 +0700

    Create tuongtv.lua

---
+++ tuongtv.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/thanh6a5dep-maker/a/refs/heads/main/truongzizi.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/thanh6a5dep-maker/a/refs/heads/main/bloxfruit.lua'))()
